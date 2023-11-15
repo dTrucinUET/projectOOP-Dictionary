34d7254dcc4aa3f675453204ffce0224aec4fcf3
Wookie <2202817@vnu.edu.vn>
2023-11-15 23:21:43 +0700

    add main class

---
+++ src/main/java/com/example/dictionary/App.java
@@ -0,0 +1,27 @@
+package com.example.dictionary;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * Main Class để run
+ */
+public class App extends Application {
+    public App() {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(final Stage stage) throws Exception {
+
+    }
+}
--- src/main/java/com/example/dictionary/Application.java
+++
@@ -1,7 +0,0 @@
-package com.example.dictionary;
-
-/**
- * Class Application la main class de run
- */
-public class Application {
-}
