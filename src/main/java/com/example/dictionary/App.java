5fc4eb177cb8e6104a21b3e9b073b23997da0291
Wookie <2202817@vnu.edu.vn>
2023-11-16 01:52:37 +0700

    des home

--- src/main/java/com/example/dictionary/App.java
+++ src/main/java/com/example/dictionary/App.java
@@ -25,44 +25,10 @@ public class App extends Application {
     }
 
     public void start(Stage stage) throws Exception {
-        System.out.println("hi");
-        AnchorPane root = new AnchorPane();
-        Button button1 = new Button("(B1) Top + Left + Right");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 960, 640);
+        stage.setTitle("Dictionary");
 
-        Button button2 = new Button("(B2) Top + Left + Right");
-        Button button3 = new Button("(B3) Top + Left + Right");
-
-        Button button4 = new Button("(B4) Top + Left + Right + Bottom");
-
-        // (B1) Neo vào Top + Left + Right
-        AnchorPane.setTopAnchor(button1, 40.0);
-        AnchorPane.setLeftAnchor(button1, 50.0);
-        AnchorPane.setRightAnchor(button1, 70.0);
-
-        // (B2) Neo vào Top + Left + Right
-        AnchorPane.setTopAnchor(button2, 90.0);
-        AnchorPane.setLeftAnchor(button2, 10.0);
-        AnchorPane.setRightAnchor(button2, 320.0);
-
-
-        // (B3) Neo vào Top + Left + Right
-        AnchorPane.setTopAnchor(button3, 100.0);
-        AnchorPane.setLeftAnchor(button3, 250.0);
-        AnchorPane.setRightAnchor(button3, 20.0);
-
-        // (B4) Neo vào 4 cạnh của AnchorPane
-        AnchorPane.setTopAnchor(button4, 150.0);
-        AnchorPane.setLeftAnchor(button4, 40.0);
-        AnchorPane.setRightAnchor(button4, 50.0);
-        AnchorPane.setBottomAnchor(button4, 45.0);
-
-
-        // Thêm vào AnchorPane
-        root.getChildren().addAll(button1, button3, button2, button4);
-
-        Scene scene = new Scene(root, 550, 250);
-
-        stage.setTitle("AnchorPane Layout Demo (o7planning.org)");
         stage.setScene(scene);
         stage.show();
     }
